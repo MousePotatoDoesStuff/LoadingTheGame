766033ea8b98e2c0fd40aaeeeb0091ecc566b23f
Dorijan Cirkveni <mousepotato@tutanota.com>
2024-07-13 21:57:55 +0200

    I'm back

--- Scenes/util/AudioController/AudioController.gd
+++ Scenes/util/AudioController/AudioController.gd
@@ -1,24 +1,48 @@
 extends Control
 
-
+var AUDIOPATH="res://Assets/Audio/"
 var audio_dict={}
-var playing={}
-var looping={}
+var musicPlaying=null
 var bus={}
+var players={}
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for child in get_children():
-		for node in child.get_children():
-			if node is AudioStreamPlayer:
-				audio_dict[node.name] = node
-				var curcall=Callable(self, "checkLoop")
-				var autocall=curcall.bindv([node.name])
-				node.connect("finished", autocall)
-
+	players={
+		'music':$MusicPlayer,
+		'sound':$SoundPlayer
+	}
+	import_audio_files()
+	play_audio_file('blipSelect','music')
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func import_audio_files(filepath=AUDIOPATH):
+	var dir = DirAccess.open(filepath)
+	if not dir:
+		print("Audio import error!")
+		return
+	dir.list_dir_begin()
+	var file_name=dir.get_next()
+	while file_name:
+		if file_name.ends_with(".wav") or file_name.ends_with(".ogg") or file_name.ends_with(".mp3"):
+			var full_path=filepath + "/" + file_name
+			var musicname=file_name.substr(0,file_name.length()-4)
+			audio_dict[musicname]=full_path
+		elif dir.current_is_dir():
+			import_audio_files(filepath+"/"+file_name)
+		file_name = dir.get_next()
+	dir.list_dir_end()
+
+func play_audio_file(file_name:String,mode:String):
+	if mode not in players:
+		print('Audio mode %s invalid'.format(mode))
+		return
+	var player=players[mode]
+	var file_path=audio_dict[file_name]
+	$MusicPlayer.stream=load(file_path)
+	$MusicPlayer.play()
 
 func get_input_pass(input:Dictionary):
 	if "audio" not in input:
@@ -50,29 +74,20 @@ func handle_audio(COM:Array[String]):
 		mode="stop"
 	if mode=="play":
 		var sel_audio=audio_dict[sel_music]
-		var cur=playing.get(sel_bus,null)
+		var cur=null
 		if cur==sel_audio and "force" not in COM:
 			return
-		playing[sel_bus]=sel_audio
 		var islooping=sel_bus in ['music']
 		if "loop" in COM:
 			islooping=true
 		if "noloop" in COM:
 			islooping=false
-		looping[sel_music]=islooping
-		sel_audio.play()
+		print("Play")
 	elif mode=="stop":
-		var sel_audio=playing[sel_bus]
-		looping[sel_bus]=false
-		playing[sel_bus]=null
-		sel_audio.stop()
+		print("Stop")
 
-func checkLoop(sel_music:String):
-	if looping.get(sel_music,false):
-		var sel_audio=audio_dict[sel_music]
-		sel_audio.play()
-	else:
-		playing[sel_bus]=null
+func checkLoop(source:String):
+	pass
 
 func test():
 	print("TEST")
