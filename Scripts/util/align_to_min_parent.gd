ff678369d21aad4e987a7efa64e208b6666df3a4
Dorijan Cirkveni <mousepotato@tutanota.com>
2024-07-16 12:49:32 +0200

    Update align_to_min_parent.gd

--- Scripts/util/align_to_min_parent.gd
+++ Scripts/util/align_to_min_parent.gd
@@ -8,8 +8,6 @@ func _ready():
 func _process(delta):
 	var parent_size = get_parent().size
 	var ratio=parent_size/size
-	print(ratio)
 	var minratio=min(ratio.x,ratio.y)
 	size*=minratio
 	position=(parent_size-size)/2
-	pass
